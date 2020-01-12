707a2c7d211d31437f6564e11c8ab6a03c21bb94
Sophie <51367731+KittyNighthawk@users.noreply.github.com>
2020-01-12 22:30:23 +0000

    Initial commit of file
    Initial commit of EnumerateClasses.js Frida script

---
+++ ios/EnumerateClasses.js
@@ -0,0 +1,38 @@
+/*
+ * Title: EnumerateClasses
+ *
+ * Description: This Frida script hooks all the classes of an application and prints their names to console. It produces a lot of
+ * output as it does not remove standard iOS SDK classes.
+ *
+ * Created by Kitty Nighthawk 2020
+ */
+
+ // Constant values for the ANSI colour codes
+ const reset = "\x1b[0m";
+ const black = "\u001b[30m";
+ const red = "\u001b[31m";
+ const green = "\u001b[32m";
+ const yellow = "\u001b[33m";
+ const blue = "\u001b[34m";
+ const magenta = "\u001b[35m";
+ const cyan = "\u001b[36m";
+ const white = "\u001b[37m";
+ const bold = "\u001b[1m";
+
+console.log(magenta, "[*] Starting list classes script", reset);
+
+if(ObjC.available) {
+  try {
+    var classes = [];
+
+    for (var class_name in ObjC.classes) {
+      console.log(bold, green, "[+] Class found: ", class_name, reset);
+    }
+  } catch(err) {
+    console.log(red, "[!] An exception occured: ", reset, err.message);
+  }
+} else {
+  console.log(red, "[-] Objective-C runtime is not available.", reset);
+}
+
+console.log(cyan, "[*] LAContext authentication bypass is now running", reset);
